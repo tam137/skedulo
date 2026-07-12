45b0ad2301ab44185a2faccb4d9cbf2d5d2ac9a0
tam137 <tam137@live.de>
2026-07-12 11:41:23 +0200

    Fix: correct relative import path in CustomMultiSelect.js

--- src/js/components/CustomMultiSelect.js
+++ src/js/components/CustomMultiSelect.js
@@ -1,4 +1,4 @@
-import { escapeHtml } from './utils.js';
+import { escapeHtml } from '../utils.js';
 
     class CustomMultiSelect {
         constructor(elementId, placeholderText = 'Benutzer auswählen...') {
